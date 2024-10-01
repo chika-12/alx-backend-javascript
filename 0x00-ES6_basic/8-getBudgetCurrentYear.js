47e7ca3fa714ff5c26dc68099b7e4263d6a0e61a
chika-12 <markworship001@gmail.com>
2024-10-01 15:55:28 +0100

    backend

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,17 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const budget = {};
+  const money = 'income-';
+  const val = 'gdp-';
+  const number = 'capita-';
+
+  budget[`${money}${getCurrentYear()}`] = income;
+  budget[`${val}${getCurrentYear()}`] = gdp;
+  budget[`${number}${getCurrentYear()}`] = capita;
+
+  return budget;
+}
