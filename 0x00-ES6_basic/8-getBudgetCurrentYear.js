b3e8b2c1f273de889ff856000b5707a9de63a7eb
chika-12 <markworship001@gmail.com>
2024-10-01 15:57:43 +0100

    backend

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,13 +5,13 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {};
-  const money = 'income-';
-  const val = 'gdp-';
-  const number = 'capita-';
+  const money = 'income';
+  const val = 'gdp';
+  const number = 'capita';
 
-  budget[`${money}${getCurrentYear()}`] = income;
-  budget[`${val}${getCurrentYear()}`] = gdp;
-  budget[`${number}${getCurrentYear()}`] = capita;
+  budget[`${money}-${getCurrentYear()}`] = income;
+  budget[`${val}-${getCurrentYear()}`] = gdp;
+  budget[`${number}-${getCurrentYear()}`] = capita;
 
   return budget;
 }
