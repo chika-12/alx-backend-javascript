bf7fe2dc671260b8bb20e21b518f6a02981721e4
chika-12 <markworship001@gmail.com>
2024-10-05 03:37:45 +0100

    backend

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -2,6 +2,9 @@ import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
     this._amount = amount;
     this._currency = currency;
   }
