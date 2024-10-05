f3986fbd457328d2afc68fb510c66fd8cc44237f
chika-12 <markworship001@gmail.com>
2024-10-05 03:31:29 +0100

    backend

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    this._amount = value;
+  }
+
+  set currency(num) {
+    this._currency = num;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    const fullConvert = amount * conversionRate;
+    return fullConvert;
+  }
+}
