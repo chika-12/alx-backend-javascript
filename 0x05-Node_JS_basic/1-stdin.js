ba124780de276d50996f50e88e56645648b11baa
chika-12 <markworship001@gmail.com>
2024-12-02 01:25:45 +0100

    js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,18 @@
-const readline = require('readline');
+if (require.main === module) {
+  process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const r1 = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-r1.question('Welcome to Holberton School, what is your name? ', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  r1.close();
-});
+  if (process.stdin.isTTY) {
+    process.stdin.on('data', (data) => {
+      process.stdout.write(`Your name is: ${data.toString()}`);
+      process.exit();
+    });
+  } else {
+    process.stdin.on('data', (data) => {
+      process.stdout.write(`Your name is: ${data.toString()}`);
+      process.exit();
+    });
+    process.on('exit', () => {
+      process.stdout.write('This important software is now closing\n');
+    });
+  }
+}
